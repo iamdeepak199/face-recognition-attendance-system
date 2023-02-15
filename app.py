548b85284c91e44c2e99e9b354acb2fd2e9edf78
Deepak bhardwaj <80276722+iamdeepak199@users.noreply.github.com>
2023-02-15 22:40:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ def start():
     if 'face_recognition_model.pkl' not in os.listdir('static'):
         return render_template('home.html',totalreg=totalreg(),datetoday2=datetoday2,mess='There is no trained model in the static folder. Please add a new face to continue.') 
 
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(1)
     ret = True
     while ret:
         ret,frame = cap.read()
